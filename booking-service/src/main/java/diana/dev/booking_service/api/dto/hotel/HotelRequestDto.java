d0d354c57b978d6c9aa2e742e6efce5172370ca5
Diana Radchuk <dupshur@bk.ru>
2026-07-07 01:09:12 +0300

    feat: added rooms field to HotelResponseDto and mapstruct dependency, and validation for HotelRequestDto

--- booking-service/src/main/java/diana/dev/booking_service/api/dto/hotel/HotelRequestDto.java
+++ booking-service/src/main/java/diana/dev/booking_service/api/dto/hotel/HotelRequestDto.java
@@ -1,7 +1,12 @@
 package diana.dev.booking_service.api.dto.hotel;
 
+import jakarta.validation.constraints.NotNull;
+
 public record HotelRequestDto(
+        @NotNull
         String name,
+
+        @NotNull
         String address
 ) {
 }
--- booking-service/src/main/java/diana/dev/booking_service/api/dto/hotel/HotelResponseDto.java
+++ booking-service/src/main/java/diana/dev/booking_service/api/dto/hotel/HotelResponseDto.java
@@ -1,8 +1,13 @@
 package diana.dev.booking_service.api.dto.hotel;
 
+import diana.dev.booking_service.api.dto.room.RoomResponseDto;
+
+import java.util.List;
+
 public record HotelResponseDto(
         Long id,
         String name,
-        String address
+        String address,
+        List<RoomResponseDto> rooms
 ) {
 }
