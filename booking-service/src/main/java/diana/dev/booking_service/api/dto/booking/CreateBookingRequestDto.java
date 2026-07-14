0ab407d118884041deb42b8111935dea5f8bbdc5
Diana Radchuk <dupshur@bk.ru>
2026-07-14 16:26:59 +0300

    feat: added CreateBookingRequestDto.java validation

--- booking-service/src/main/java/diana/dev/booking_service/api/dto/booking/CreateBookingRequestDto.java
+++ booking-service/src/main/java/diana/dev/booking_service/api/dto/booking/CreateBookingRequestDto.java
@@ -1,11 +1,27 @@
 package diana.dev.booking_service.api.dto.booking;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDate;
 
 public record CreateBookingRequestDto(
+    @NotNull
     Long roomId,
+
+    @NotNull
+    @Positive
     Integer guests,
+
+    @NotNull
+    @FutureOrPresent
     LocalDate checkInDate,
+
+    @NotNull
+    @Future
     LocalDate checkOutDate,
+
     String bookingPreferences
 ) { }
