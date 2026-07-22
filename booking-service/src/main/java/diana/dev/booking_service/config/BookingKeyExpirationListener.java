3fdaadb352bbc89594e7af780b41f45b5d8c0f0b
Diana Radchuk <dupshur@bk.ru>
2026-07-22 16:59:23 +0300

    feat: add redis event listener to cancel the reservation after 15 minutes of non-payment

---
+++ booking-service/src/main/java/diana/dev/booking_service/config/BookingKeyExpirationListener.java
@@ -0,0 +1,41 @@
+package diana.dev.booking_service.config;
+
+import diana.dev.booking_service.api.dto.booking.BookingDto;
+import diana.dev.booking_service.domain.BookingService;
+import diana.dev.booking_service.domain.db.entity.BookingEntity;
+import diana.dev.booking_service.domain.db.repository.BookingRepository;
+import diana.dev.shared.http.booking.BookingStatus;
+import org.jspecify.annotations.Nullable;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class BookingKeyExpirationListener extends KeyExpirationEventMessageListener {
+
+    private final BookingRepository repository;
+
+    public BookingKeyExpirationListener(RedisMessageListenerContainer listenerContainer, BookingService service, BookingRepository repository) {
+        super(listenerContainer);
+        this.repository = repository;
+    }
+
+    @Override
+    public void onMessage(Message message, byte @Nullable [] pattern) {
+        String msg = message.toString();
+        if (msg.startsWith("booking:expired:")) {
+            Long bookingId = Long.valueOf(msg.substring(16));
+
+            repository.findById(bookingId).ifPresent(booking -> {
+                if (booking.getStatus().equals(BookingStatus.PENDING_PAYMENT)) {
+                    booking.setStatus(BookingStatus.CANCELLED);
+                    repository.save(booking);
+                }
+            });
+
+        }
+    }
+}
---
+++ booking-service/src/main/java/diana/dev/booking_service/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package diana.dev.booking_service.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer (
+            RedisConnectionFactory redisConnectionFactory
+    ) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        return container;
+    }
+
+}
