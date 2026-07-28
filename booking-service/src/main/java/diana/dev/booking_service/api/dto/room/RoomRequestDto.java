e0334df8a9c5286602b8b5e5d27b689f9b3e97da
Diana Radchuk <dupshur@bk.ru>
2026-07-28 21:22:19 +0300

    refactor: change room number's NotNull to NotBlank annotation

--- booking-service/src/main/java/diana/dev/booking_service/api/dto/room/RoomRequestDto.java
+++ booking-service/src/main/java/diana/dev/booking_service/api/dto/room/RoomRequestDto.java
@@ -1,12 +1,13 @@
 package diana.dev.booking_service.api.dto.room;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
 public record RoomRequestDto (
-        @NotNull(message = "Room number is required")
+        @NotBlank(message = "Room number is required")
         String number,
 
         @NotNull(message = "Price per night is required")
